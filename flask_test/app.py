1e193bcdd4953ff0d01678016cb394f8a7f3cad6
Ian Forrest <ianforrest11@gmail.com>
2019-12-03 15:09:16 -0500

    flask app test push

--- flask_test/app.py
+++ flask_test/app.py
@@ -1,25 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# define route for home page
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
-DB = SQLAlchemy(app)
+# define route for about page
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-class Business(DB.Model):
-    """Create class 'Business' for Yelp App
-    Business has methods:
-    - id
-    - name
-    - location
+# define route for businesses page
+@app.route("/businesses")
+def businesses():
+    return "<h1>Businesses Page</h1>"
+
+# allow app to run using "python app.py" in terminal
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# DB = SQLAlchemy(app)
+
+# class Business(DB.Model):
+#     """Create class 'Business' for Yelp App
+#     Business has methods:
+#     - id
+#     - name
+#     - location
     
-    added __repr__ function to show content of Business class as text (vs. as location in memory)"""
+#     added __repr__ function to show content of Business class as text (vs. as location in memory)"""
     
-    id = DB.Column(DB.String, 
-                   primary_key = True)
-    name = DB.Column(DB.String(100), 
-                     nullable = False)
-    business_location = DB.Column(DB.String(100))
+#     id = DB.Column(DB.String, 
+#                    primary_key = True)
+#     name = DB.Column(DB.String(100), 
+#                      nullable = False)
+#     business_location = DB.Column(DB.String(100))
