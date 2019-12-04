ee2439a0ed95a18f0285c443ea4540140297145e
Ian Forrest <ianforrest11@gmail.com>
2019-12-04 15:47:34 -0500

    successfully simulated dummy login

--- flask_test/app.py
+++ flask_test/app.py
@@ -52,6 +52,12 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 # define route for manual review entry
